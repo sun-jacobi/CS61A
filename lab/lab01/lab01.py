424c01c7f4d2bb54ed0e55136c75d733bbf2ea4e
sun-jacobi <sun1011jacobi@gmail.com>
2022-02-08 12:54:12 +0900

    lab 01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,66 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """ 
+    res = 1
+    for i in range(k):
+        res *= n-i
+    return res
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    res = 0
+    dg = 10
+    while not y == 0:
+        res += y % 10
+        y //= 10
+    return res
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    time = 0 
+    while not n == 0:
+        dg = n % 10 
+        if dg == 8 :
+            time += 1
+        else : 
+            time = 0
+        if time >= 2 :
+            return True
+        n //= 10
+    return False
+
