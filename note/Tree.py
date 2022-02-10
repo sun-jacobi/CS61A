6db22244e692e39a67357f76aad009c31d4a1bff
sun-jacobi <sun1011jacobi@gmail.com>
2022-02-10 17:41:12 +0900

    hw04

--- note/Tree.py
+++ note/Tree.py
@@ -36,3 +36,22 @@ def partition_tree(n,m):
         left = partition_tree(n-m,m)
         right = partition_tree(n,m-1)
         return tree(m,[left,right])
+    
+def print_parts(tree,partition=[]):
+    if is_leaf(tree):
+        if label(tree):
+            print('+'.join(partition))
+    else:
+        left, right = branches(tree)
+        m = str(label(tree))
+        print_parts(left,partition + [m])
+        print_parts(left,partition)
+
+def right_binarize(tree):
+    if is_leaf(tree):
+        return tree
+    if len(tree) > 2 : 
+        tree = [tree[0],tree[1:]]
+    return [right_binarize(b) for b in tree]
+        
+            
