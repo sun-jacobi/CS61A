4d5c473e449c9e8973191e34c3c9494488a4fff1
sun-jacobi <sun1011jacobi@gmail.com>
2022-02-19 16:48:41 +0900

    hw 07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,48 +1,56 @@
-(define (cddr s)
-  (cdr (cdr s)))
+(define (cddr s) (cdr (cdr s)))
 
-(define (cadr s)
-  'YOUR-CODE-HERE
-)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s)
-  'YOUR-CODE-HERE
-)
-
+(define (caddr s) (car (cdr (cdr s))))
 
 (define (sign num)
-  'YOUR-CODE-HERE
-)
-
+  (cond 
+    ((> num 0) 1)
+    ((= num 0) 0)
+    ((< num 0) -1)))
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
-)
-
+  (if (= y 1)
+      x
+      (if (even? y)
+          (* (square (pow x (/ y 2))))
+          (* (square (pow x (/ (- y 1) 2))) x))))
 
 (define (unique s)
-  'YOUR-CODE-HERE
-)
-
+  (if (null? s)
+      ()
+      (cons (car s)
+            (unique (filter (lambda (x) (not (eq? x (car s))))
+                            (cdr s))))))
 
 (define (replicate x n)
-  'YOUR-CODE-HERE
-  )
-
+  (define (helper n cache)
+    (if (= 0 n)
+        cache
+        (helper (- n 1) (cons x cache))))
+  (helper n nil))
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
-)
-
+  (define (helper t n cache)
+    (if (= n 0)
+        cache
+        (helper (+ t 1) (- n 1) (combiner cache (term t)))))
+  (helper 1 n start))
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
-)
+  (define (helper t n cache)
+    (if (= n 0)
+        cache
+        (helper (+ t 1) (- n 1) (combiner cache (term t)))))
+  (helper 1 n start))
 
-
-(define-macro (list-of map-expr for var in lst if filter-expr)
-  'YOUR-CODE-HERE
-)
-
+(define-macro
+ (list-of map-expr for var in lst if filter-expr)
+ (list 'map
+       (list 'lambda (list var) map-expr)
+       (list 'filter
+             (list 'lambda (list var) filter-expr)
+             lst)))
